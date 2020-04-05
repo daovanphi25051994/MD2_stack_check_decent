fba4668ead3131417b789423c4339b82ea696995
phi <daovanphi25051994@gmail.com>
2020-04-05 11:56:26 +0700

    ok

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string : ");
+        String inputString = scanner.nextLine();
+        if (isDecent(inputString)) {
+            System.out.println("decent!!");
+        } else {
+            System.out.println("invalid");
+        }
+    }
+
+    public static boolean isDecent(String string) {
+        Stack<String> listChar = new Stack<>();
+        for (int i = 0; i < string.length(); i++) {
+            if (string.substring(i, i + 1).equals("(")) {
+                listChar.push(string.substring(i, i + 1));
+            }
+            if (string.substring(i, i + 1).equals(")")) {
+                if (listChar.isEmpty()) {
+                    return false;
+                }
+                listChar.pop();
+            }
+        }
+        if (listChar.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
